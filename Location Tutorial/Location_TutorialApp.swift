a80c0253510fa051ac12a77ee1f4cadcbb9b731e
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-11-13 22:48:23 +0300

    Initial Commit

---
+++ Location Tutorial/Location_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_TutorialApp.swift
+//  Location Tutorial
+//
+//  Created by Fatih Durmaz on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Location_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
